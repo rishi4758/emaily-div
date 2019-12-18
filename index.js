e1e48e0e7ff952d7d9331123d5269999db6415b4
rishav rapta <rishavrapta79@gmail.com>
2019-12-18 15:46:21 +0530

    go

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const passport=require("passport");
+const strategy=require("passport-google-oauth20").Strategy;
+const key=require("./config/keys")
 const app=express();
-// cnbckjndkjshk
-app.get('/',(req,res)=>{
-
-res.send({ name:'rishav'})
-
+console.log(key.googleclientSecret)
+passport.use( new strategy(
+{clientID: key.googleclientID,
+clientSecret:key.googleclientSecret,
+callbackURL:'/auth/google/callback'
+},(token)=>{
+console.log(token)
 })
+);
+
+app.get('/auth/google/',
+passport.authenticate('google',{
+    scope:['profile','email']
+}))
+app.get('/',(req,res)=>{
+    res.send({ name:"risahv"})
+}
+)
+
 const port=process.env.PORT || 3000;
 app.listen(port)

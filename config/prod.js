9a8820934193688c3918965eb6adbf838c0ad082
rishav rapta <rishavrapta79@gmail.com>
2019-12-26 02:51:41 +0530

    kr do

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,5 @@ module.exports = {
     googleclientSecret:process.env.GOOGLE_CLIENT_SECRET,
     mongoURI:process.env.MONGO_URI,
     cookie_key:process.env.COOKEY_KEY,
-    
+    googleRedirectURI:'https://rocky-forest-87228.herokuapp.com'
 }//GH5Xy42Yc75tWthd

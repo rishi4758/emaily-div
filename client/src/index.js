72b422f0f1266bcb75ac911d021936000808c7e2
rishav rapta <rishavrapta79@gmail.com>
2020-01-16 17:15:05 +0530

    add buld

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,10 @@
 import 'materialize-css/dist/css/materialize.min.css'
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./app.js"
+import App from "app.js"
 import {Provider} from 'react-redux';
 import {createStore,applyMiddleware} from 'redux';
 import reducers from './reducers';
 import reduxthunk from 'redux-thunk'
 const store=createStore(reducers,{},applyMiddleware(reduxthunk));
 ReactDOM.render(<Provider store={store}> <App/></Provider>,document.getElementById("root"));
-
-console.log(process.env.NODE_ENV)
-console.log(process.env.REACT_APP_STRIPEKEY)

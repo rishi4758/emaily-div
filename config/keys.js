1a83f7d76b1fc8399ff3215cc8a8a49137e61fe4
rishav <myemail>
2020-10-23 16:41:50 +0530

    heroku update

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-if(process.env.NODE_ENV=='production'){
-module.exports=require("./prod.js")
-
-}else{
-    module.exports=require("./dev.js")
+if (process.env.NODE_ENV == "production") {
+  module.exports = require("./dev.js");
+} else {
+  module.exports = require("./dev.js");
 }

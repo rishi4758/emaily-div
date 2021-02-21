fbee77bdfcc470adbd3d1db5aa1f161d4342084c
rishav <rishavrapta79@gmail.com>
2021-02-21 16:19:00 +0530

    few changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,29 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./components/Header.js";
+import landing from "./components/landing.js";
+import SurveyNew from "./components/surveys/surveynew";
+import { connect } from "react-redux";
+import * as actions from "./actions";
+import Dashboard from "./components/dashboard";
 
-import React ,{Component} from "react"
-import {BrowserRouter,Route} from 'react-router-dom';
-import Header from './components/Header.js'
-import landing from './components/landing.js'
-import SurveyNew from './components/surveys/surveynew'
-import {connect} from 'react-redux';
-import * as actions from './actions'
-import Dashboard from './components/dashboard'
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
 
-    
-     
-class App extends Component{
-     componentDidMount(){
-this.props.fetchUser();
-     }
-     render(){
-    return(  
-    <div className="container">
-         <BrowserRouter>
-   <div>
-        <Header/>
-        <Route  exact  path ="/" component ={landing}></Route>
-        <Route exact path="/surveys" component={Dashboard}></Route>
-        <Route exact path="/surveys/new" component={SurveyNew}></Route>
-</div>      
-         </BrowserRouter>
-
-    </div>  
-    ) }  
-    }
-    export default  connect (null,actions)(App);
+            <Route exact path="/" component={Dashboard}></Route>
+            <Route exact path="/surveys/new" component={SurveyNew}></Route>
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+export default connect(null, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,6 @@
-import 'materialize-css/dist/css/materialize.min.css'
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App.js"
-import {Provider} from 'react-redux';
-import {createStore,applyMiddleware} from 'redux';
-import reducers from './reducers';
-import reduxthunk from 'redux-thunk'
-import axios from 'axios'
-window.axios=axios;
-const store=createStore(reducers,{},applyMiddleware(reduxthunk));
-ReactDOM.render(<Provider store={store}> <App/></Provider>,document.getElementById("root"));
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+window.axios = axios;
+const store = createStore(reducers, {}, applyMiddleware(reduxthunk));
+ReactDOM.render(<App />, document.getElementById("root"));
